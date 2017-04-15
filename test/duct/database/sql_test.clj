d18cf3b94253d0b4efb378806df2a9470fb5a986
James Reeves <jreeves@weavejester.com>
2017-04-15 02:07:19 +0100

    Add basic tests

--- test/duct/database/sql_test.clj
+++ test/duct/database/sql_test.clj
@@ -1,7 +1,10 @@
 (ns duct.database.sql-test
   (:require [clojure.test :refer :all]
-            [duct.database.sql :refer :all]))
+            [duct.database.sql :as sql]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest integrant-key-test
+  (let [spec {:connection-uri "jdbc:sqlite:"}
+        impl (:duct.database/sql (ig/init {:duct.database/sql spec}))]
+    (is (instance? duct.database.sql.Boundary impl))
+    (is (= (:spec impl) spec))))
