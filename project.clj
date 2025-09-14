42aa5ddbb43e08991ad763a34d42a2212e0edd13
James Reeves <jreeves@weavejester.com>
2025-09-14 17:53:10 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/duct-framework/database.sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
-                 [com.github.seancorfield/next.jdbc "1.3.955"]
-                 [integrant "0.13.1"]])
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [com.github.seancorfield/next.jdbc "1.3.1070"]
+                 [integrant "1.0.0"]])
